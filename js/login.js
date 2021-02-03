ebc4ff7efd7c22ebe055e8da2df64e629ceb8be3
Shawn Lefebre <shmebula@yahoo.com>
2021-02-02 23:30:05 -0500

    Initial version of login and registration page and code

---
+++ js/login.js
@@ -0,0 +1,31 @@
+// Adapted from https://www.formget.com/javascript-login-form/
+
+/*
+loginStorage is a dictionary (Python) where the key is the user name and it contains a dict with:
+ - password: clear text password
+ - TBD
+*/
+
+/**
+ * Log in a Budget App user with their user name and password.  Validates that the
+ * username and password are valid based on stored user data in the localStorage of
+ * the user's browser.
+ */
+function validate() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    let loginStorage = window.localStorage.getItem('loginStorage')
+    if (!loginStorage) {
+        loginStorage = {}
+    } else {
+        loginStorage = JSON.parse(loginStorage)
+    }
+
+    if (loginStorage[username] && password === loginStorage[username]['password']) {
+        alert("Login successfully")
+        window.location = "success.html"
+        return false;
+    } else {
+        alert("Invalid Login");
+    }
+}
---
+++ js/register.js
@@ -0,0 +1,25 @@
+/**
+ * Register a Budget App user with their user name and password.  Validates that the
+ * username is not already taken.  Stores user data in the localStorage of the user's
+ * browser.
+ */
+function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    let loginStorage = window.localStorage.getItem('loginStorage')
+    if (!loginStorage) {
+        loginStorage = {}
+    } else {
+        loginStorage = JSON.parse(loginStorage)
+    }
+
+    // if (loginStorage[username]) {
+    if (username in loginStorage) {
+        alert(`User ${username} already has an account.`)
+    } else {
+        loginStorage[username] = {}
+        loginStorage[username]['password'] = password
+        window.localStorage.setItem('loginStorage', JSON.stringify(loginStorage))
+    }
+}
